9dcf57f60a9e5d9a8090ea793c2904bd7d6402bd
phakin <phakin.neamson@gmail.com>
2022-05-31 01:45:27 +0700

    Register And Login

--- server/routes/register/index.js
+++ server/routes/register/index.js
@@ -6,20 +6,38 @@ const uri = `mongodb://${process.env.DB_USER}:${process.env.DB_PWD}@${process.en
 const register = async (req, res) => {
     let { username, password, cPassword, email } = req.body;
     username = username.toLowerCase()
+    
     let query = { username };
     let status, code
     const client = new MongoClient(uri);
     await client.connect();
     let user = await client.db(`${process.env.db_name}`).collection("users").findOne(query);
     if (!user) {
+
+//-----------------------------------------------------------------------------------------------------        
+        if(username.length<5){
+            status = "Unsuccess"
+            msg = "Username must have at least 5 charactors" //Username length check
+            code = 400
+        }
+ //-----------------------------------------------------------------------------------------------------  
+
+        else{
         let emailQuery = { email }
         emailQuery = await client.db(`${process.env.db_name}`).collection("users").findOne(emailQuery);
         if (!emailQuery) {
-            if (password == cPassword) {
+//------------------------------------------------------------------------------------------------------
+            if(password.length<8){
+                status = "Unsuccess"
+                msg = "Password must have at least 8 charactors"
+                code = 400
+            }
+//------------------------------------------------------------------------------------------------------
+            else if (password == cPassword) {
                 let salt = crypto.randomBytes(16).toString('hex')
                 let hashPassword = crypto.pbkdf2Sync(password, salt,
                     1000, 64, `sha512`).toString(`hex`);
-                let doc = { username, email, hashPassword, salt, admin: false }
+                let doc = { username, email, hashPassword, salt, admin: true }
                 await client.db(`${process.env.db_name}`).collection("users").insertOne(doc);
                 status = "Success"
                 msg = "Register Successful"
@@ -36,7 +54,7 @@ const register = async (req, res) => {
             status = "Unsuccess"
             msg = "Email already exist"
             code = 400
-        }
+        }}
 
     }
     else {
--- website/src/pages/register.js
+++ website/src/pages/register.js
@@ -1,5 +1,6 @@
-import React , {useState,useEffect} from 'react'
+import React , { useState } from 'react'
 import styled from 'styled-components';
+import { AiFillEyeInvisible , AiOutlineEye } from 'react-icons/ai';
 import { Link , Navigate } from 'react-router-dom';
 import axios from 'axios';
 
@@ -15,13 +16,23 @@ const Container = styled.div`
     text-align: left;
 `
 function Register() {
-    const [succes,setSucess] = useState(false);
-    const [pstatus,setPstatus] = useState(true)
-    const [user,setUser] = useState(true);
-    const [mail,setMail] = useState(true);
-    const [pass,setPass] = useState(true);
-    const [error,setError] = useState("");
+    const [succes,setSucess] = useState(false);  // Navigate to "/login"
 
+    const [errorUserName,setErrorUserName] = useState('');
+    const [errorEmail,setErrorEmail] = useState('');
+    const [errorPassWord,setErrorPassWord] = useState('');
+    const [errorRePassword,setErrorRePassWord] = useState('');
+    
+    const [userNameColor,setUserNameColor] = useState('');
+    const [emailColor,setEmailColor] = useState('');
+    const [passwordColor,setPasswordColor] = useState('');
+    const [rePassWordColor,setRePassWordColor] = useState('');
+
+    const [showPassword,setShowPassword] = useState(false);
+
+    const passwordVisibility = ()=>{
+        setShowPassword(!showPassword);
+    }
 
     const handleSubmit = (e)=>{
         // console.log(e);
@@ -32,7 +43,33 @@ function Register() {
         let cPassword = e.target.cpassword.value;
         const url = `${ip}:${port}/register`;
 
-        setPstatus(true);
+        if(username.length < 5){
+            setErrorUserName("Username must have at least 5 charactors");
+            setUserNameColor("red");
+        }
+        else{
+            setErrorUserName("");
+            setUserNameColor("green");
+        }
+
+        if(password.length < 8){
+            setErrorPassWord("Password must have at least 8 charactors");
+            setPasswordColor("red");
+            setRePassWordColor("red");
+        }
+        else if(password !== cPassword){
+            setErrorPassWord("")
+            setErrorRePassWord("Password and Confirm password doesn't match");
+            setPasswordColor("red");
+            setRePassWordColor("red");
+            return
+        }
+        else{
+            setPasswordColor("green");
+            setRePassWordColor("green");
+            setErrorRePassWord("");
+        }
+       
         axios.post(url,{
             username,
             password,
@@ -43,108 +80,120 @@ function Register() {
             console.log(res.data.msg);
             setSucess(true);
         }))
-        .catch((err)=>{
-            setPstatus(false);
-            console.log(err.response.data.msg);
+        .catch((err)=>{  
+            console.log(err.response.data);
             if(err.response.data.msg === "Username already exist"){
-                setUser(false);
+                setErrorUserName("Username already exist");
+                setUserNameColor('red');
             }
-            else if(err.response.data.msg === "Email already exist"){
-                setMail(false);
+
+            if(err.response.data.msg === "Email already exist"){
+                setErrorEmail("Email already exist");
+                setEmailColor('red');         
             }
-            else if(err.response.data.msg === "Password and Confirm password doesn't match"){
-                setPass(false);
+            else{
+                setErrorEmail("");
+                setEmailColor('green');
             }
-            setError(err.response.data.msg);
-        });
-            
-        
-}
+        }); 
+    }
 
   return (
     <Container>
-            {succes && <Navigate to="/login"/>}
-            <form onSubmit={handleSubmit}>
-                <h3 style={{ textAlign: "center", padding: '1rem 0' }}>Register</h3>
+        {succes && <Navigate to="/login"/>}
+        <form onSubmit={handleSubmit}>
+            <h3 style={{ textAlign: "center", padding: '1rem 0' }}>Register</h3>
+            <div className="form-group" style={{ marginTop: "2vh" }}>
+                <Label>Username</Label> 
+                <input
+                    type="text"
+                    className="form-control"
+                    required={true}
+                    name="username"
+                    placeholder="Username"
+                    style={{ marginTop: "2vh",width:"95%",borderColor:userNameColor}}
+                />
+                <small style={{color:userNameColor}}>{errorUserName}</small>
+            </div>
 
-                <div className="form-group" style={{ marginTop: "2vh" }}>
-                    {user ?  <Label>Username</Label> : <Label style={{color:"red"}}>Username 
-                        <img src={require('../img/warning.png')} width="15px" height="15x" alt="warnning" style={{textAlign:"center",marginLeft:"5px"}}/> 
-                        </Label>}
-                    <input
-                        type="text"
-                        className="form-control"
-                        required={true}
-                        name="username"
-                        placeholder="Enter Username"
-                        style={{ marginTop: "2vh" }}
-                    />
-                </div>
+            <div className="form-group" style={{ marginTop: "2vh" }}>
+                <Label>Email</Label>
+                <input
+                    type="email"
+                    className="form-control"
+                    required={true}
+                    name="email"
+                    placeholder="Email"
+                    style={{ marginTop: "2vh",width:"95%",borderColor:emailColor}}
+                />
+                <small style={{ color:emailColor}}>{errorEmail}</small>
+            </div>
 
-                <div className="form-group" style={{ marginTop: "2vh" }}>
-                {mail ?  <Label>Email</Label> : 
-                        <Label style={{color:"red"}}>Email 
-                        <img src={require('../img/warning.png')} width="15px" height="15x" alt="warnning" style={{textAlign:"center" ,marginLeft:"5px"}}/> 
-                        </Label>}
+            <div className="form-group" style={{ marginTop: "2vh"}}>
+                <Label>Password</Label>
+                <div style={{display:"flex"}}>
                     <input
-                        type="email"
-                        className="form-control"
-                        required={true}
-                        name="email"
-                        placeholder="Enter Email"
-                        style={{ marginTop: "2vh" }}
-                    />
-                </div>
-
-                <div className="form-group" style={{ marginTop: "2vh" }}>
-                {pass?  <Label>Password</Label> : <Label style={{color:"red"}}>Password
-                        <img src={require('../img/warning.png')} width="15px" height="15x" alt="warnning" style={{textAlign:"center",marginLeft:"5px"}}/> 
-                        </Label>}
-                    <input
-                        type="password"
+                        type={!showPassword ? "password" : "text"}
                         className="form-control"
                         required={true}
                         name="password"
-                        placeholder="Enter Password"
-                        style={{ marginTop: "2vh" }}
+                        placeholder="Password"
+                        style={{ marginTop: "2vh",borderColor:passwordColor}}
                     />
+                     <div style={{marginLeft:"0.5vw"}}>
+                        {!showPassword ? <AiOutlineEye style={{fontSize:"25px" ,marginTop:"2.5vh" ,cursor:"pointer"}}
+                            onClick={passwordVisibility}
+                        /> : 
+                        <AiFillEyeInvisible style={{fontSize:"25px" ,marginTop:"2.5vh" ,cursor:"pointer"}}
+                            onClick={passwordVisibility}
+                        />}
+                    </div>
                 </div>
+                <small style={{color:passwordColor}}>{errorPassWord}</small>
+            </div>
 
-                <div className="form-group" style={{ marginTop: "2vh" }}>
-                {pass ?  <Label>Confirm Password</Label> : <Label style={{color:"red"}}>Confirm Password 
-                        <img src={require('../img/warning.png')} width="15px" height="15x" alt="warnning" style={{textAlign:"center",marginLeft:"5px"}}/> 
-                        </Label>}
+            <div className="form-group" style={{ marginTop: "2vh" }}>
+                <Label>Confirm Password</Label>
+                <div style={{display:"flex"}}>
                     <input
-                        type="password"
+                        type={!showPassword ? "password" : "text"}
                         className="form-control"
                         required={true}
                         name="cpassword"
                         placeholder="Confirm Password"
-                        style={{ marginTop: "2vh" , marginBottom:"2vh"}}
+                        style={{ marginTop: "2vh",borderColor:passwordColor}}
                     />
+                    <div style={{marginLeft:"0.5vw"}}>
+                        {!showPassword ? <AiOutlineEye style={{fontSize:"25px" ,marginTop:"2.5vh" ,cursor:"pointer"}}
+                            onClick={passwordVisibility}
+                        /> : 
+                        <AiFillEyeInvisible style={{fontSize:"25px" ,marginTop:"2.5vh" ,cursor:"pointer"}}
+                            onClick={passwordVisibility}
+                        />}
+                    </div>
                 </div>
+                <small style={{ color:rePassWordColor,marginBottom:"2vh"}}>{errorRePassword}</small>
+            </div>
 
-                <div>
-                    Already have account?, <span><Link to={"/login"}>
-                        Login
-                    </Link>
+            <div style={{marginTop:"2vh"}}>
+                Already have an account?, <span><Link to={"/login"}>
+                    Login
+                </Link>
+                </span>
+            </div>
+                
+            <button
+                type="submit"
+                className="btn btn-primary btn-block"
+                style={{ marginTop: "2vh" ,width:"95%"}}
+            >
+                Sign Up
+            </button>
 
-                    </span>
-                </div>
-                {!pstatus && <div style={{ color: 'red', paddingTop: '0.5rem', textAlign:"center" }}>
-                        {error}
-                    </div>}
-                <button
-                    type="submit"
-                    className="btn btn-primary btn-block"
-                    style={{ marginTop: "2vh" ,width:"100%"}}
-                >
-                    Sign Up
-                </button>
-
-            </form>
-        </Container>
+        </form>
+    </Container>
   )
 }
+
 
 export default Register
