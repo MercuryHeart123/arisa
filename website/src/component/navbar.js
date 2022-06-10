9fbe3dabba8587bb786cb9727678930646782955
heart <heartkung35@gmail.com>
2022-06-10 21:37:02 +0700

    ui update

--- website/src/component/navbar.js
+++ website/src/component/navbar.js
@@ -3,33 +3,58 @@ import { NavLink as Link } from 'react-router-dom';
 import styled from 'styled-components';
 import { connect } from "react-redux";
 import axios from 'axios'
-
+import { BsCircleFill } from 'react-icons/bs'
 const Nav = styled.div`
-    
     text-align: center;
-    padding: 1rem 0;
-   
 `
-const NavWrapper = styled.div`
-padding: 1rem 0;
 
+const NavLogoAndLink = styled.div`
+    display:flex;
+    position: relative;
+    width:100%;
+
+    font-family: 'Sofia', cursive !important;
+`
+
+
+const NavLogo = styled.div`
+    width:30%;
+    height: auto;
+    display: flex;
+    justify-content: center; 
+
+
+    img{
+        height:78.6px;
+        margin: 0;
+        padding: 0;
+    }
+`
+
+const NavWrapper = styled.div`
+    display: flex;
+    align-items: center;
+    position: relative;
+    width:70%;
+    
 `
 const NavLink = styled(Link)`
     color: black;
     text-decoration: none;
     margin: 0 40px;
     padding: 0 10px;
-    &.active{
+    /* &.active{
         color: gray;
-    }
+    } */
 `
 const NavAuth = styled(Link)`
     color: black;
     text-decoration: none;
-    margin: 0 40px;
-    padding: 0 10px;
+
     position: absolute;
-    right:0;
+    right: 0;
+    top: 50%;
+    transform: translate(-50%,-50%);
 `
 const NavLine = styled.div`
     padding: 0.5rem 0;
@@ -37,6 +62,27 @@ const NavLine = styled.div`
     border-bottom: 1px solid black;
     width: 90%;
     margin: 0 auto 0 auto;
+`
+
+const NavDot = styled.div`
+    opacity: 0;
+`
+
+const NavContent = styled.div`
+    display:flex;
+    flex-direction: column-reverse;
+
+    align-items: center;
+    padding-top: 20px;
+    & ${NavLink}:hover + ${NavDot} {
+        opacity: 1;
+        transition: 0.35s;
+    }
+    & ${NavLink}.active + ${NavDot} {
+        opacity: 1;
+        
+    }
+
 `
 
 const Navbar = (props) => {
@@ -51,17 +97,39 @@ const Navbar = (props) => {
     }
     return (
         <Nav>
-            <NavLink to={'/'}>Arisa.B</NavLink>
-            {!props.username && <NavAuth to={'/login'}>Login</NavAuth>}
-            {props.username && <NavAuth to={'/'} onClick={Logout}>Logout</NavAuth>}
+            <NavLogoAndLink>
+                <NavLogo>
+                    <Link to='/'>
+                        <img src='/284993175_5201268449966638_749157627880997743_n.png' />
+                    </Link>
+                </NavLogo>
+                <NavWrapper>
+                    <NavContent>
 
-            <NavWrapper>
-                <NavLink to={'/graphic-design'}>Graphic design</NavLink>
-                <NavLink to={'/interior-design'}>Interior design</NavLink>
-                <NavLink to={'/about'}>About</NavLink>
-                {props.admin && <NavLink to={'/edit'}>Edit content</NavLink>}
-                {props.admin && <NavLink to={'/editprofile'}>Edit profile</NavLink>}
-            </NavWrapper>
+                        <NavLink to={'/graphic-design'}>Graphic design</NavLink>
+                        <NavDot>
+                            <BsCircleFill style={{ color: "#C392FF" }} />
+                        </NavDot>
+                    </NavContent>
+                    <NavContent>
+                        <NavLink to={'/interior-design'}>Interior design</NavLink>
+                        <NavDot>
+                            <BsCircleFill style={{ color: "#44D19D" }} />
+                        </NavDot>
+                    </NavContent>
+                    <NavContent>
+                        <NavLink to={'/about'}>About</NavLink>
+                        <NavDot>
+                            <BsCircleFill style={{ color: "#FCDD37" }} />
+                        </NavDot>
+                    </NavContent>
+                    {props.admin && <NavLink to={'/edit'}>Edit content</NavLink>}
+                    {props.admin && <NavLink to={'/editprofile'}>Edit profile</NavLink>}
+                </NavWrapper>
+                {!props.username && <NavAuth to={'/login'}>Login</NavAuth>}
+                {props.username && <NavAuth to={'/'} onClick={Logout}>Logout</NavAuth>}
+
+            </NavLogoAndLink>
 
             <NavLine />
         </Nav>
